9e458bb5e814a5773409d69c5f385ee29446307e
TonyShikhovtsev <ssvsr2@gmail.com>
2023-11-02 22:17:05 +0500

    Внесение правок

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from catalog.views import home, contacts,  product
+from . import views
 
 urlpatterns = [
     path('', home),
     path('contacts/', contacts),
-    path('product/<int:pk>/', product, name='product_detail')
+    path('product/<int:pk>/', views.product, name='product'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render,  get_object_or_404
 from catalog.models import Product
 
-def home(requests):
+def home(request):
     product_list = Product.objects.all()
-    context = {
-        'object_list': product_list
-    }
-    return render(requests, 'catalog/home.html', context)
+    if product_list:
+        context = {
+            'products': product_list
+        }
+    else:
+        context = {
+            'products': None
+        }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(requests):
@@ -26,3 +31,4 @@ def product(request, pk):
         'product': product
     }
     return render(request, 'catalog/product.html', context)
+
